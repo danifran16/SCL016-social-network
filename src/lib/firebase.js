84b4ab27c9635a016f7d9969ed37bd45dde721cb
Rudihmar <rudihmar@gmail.com>
2021-05-10 20:27:41 -0400

    Funcionalidad de registro de nuevos usuarios

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,24 +1,37 @@
 // funcion para entrar con google
 export const googleProvider = () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
-  
-    firebase.auth().signInWithPopup(provider).then((result) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
     // /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
-  
+    const credential = result.credential;
+
     // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
+    const token = credential.accessToken;
     // The signed-in user info.
-    var user = result.user;
+    const user = result.user;
     // ...
   }).catch((error) => {
     // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    // ...
+  });
+};
+
+export const userNew = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
+      console.log('Registro Exitoso')
+    var user = userCredential.user;
+  })
+  .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+    console.log(error);
+    // ..
   });
 }
