49eb9fceaf71751278927dd9038e95f52968ee39
danifran16 <78364620+danifran16@users.noreply.github.com>
2021-06-06 16:00:35 -0400

    Orden de template en #14
    Se corrigio orden de div, falta renombrar clases en español, favor pasar a ingles

--- src/lib/views/templateRegister.js
+++ src/lib/views/templateRegister.js
@@ -1,50 +1,52 @@
 import { userNew } from '../firebase.js';
 
 export const register = () => {
-  const divRegister = document.createElement('div');
-  const viewRegister = `
-      <div class="contenedor">
-      <form id="signup-form" class="formulario">
-      <a href="#/login">
-        <i id="arrow" class="fas fa-chevron-left"></i>
-      </a>
-      <div>
-        <img class="logo" src="./imagen/logo_wup.jpg">
-      </div>
-        <p>Regístrate con tu E-mail:</p>
-        <div class="input-contenedor">
-          <i class="fas fa-envelope icon"></i>
-          <input class="input-init" type="email" id="signup-email" autocomplete="on" placeholder="E-mail" value="" required>
-        </div>
-        <div class="input-contenedor">
-          <i class="fas fa-key icon"></i>
-          <input class="input-init" type="password" id="signup-password" placeholder="Contraseña"
-            oninvalid="setCustomValidity('Tu contraseña debe tener min. 6 caracteres')" value="" required>
-        </div>
-        <div class="input-contenedor">
-        <input id="btn-register" type="submit" value="Registrarse" class="button">
-        </div>
-        <p>Ya estás registrado?<a href="#/login"> Inicia sesión aquí</a></p>
-        <p class="mess-user">Eleva tu Nivel</p>
-        <p class="mess-user">Crea Comunidad</p>
-        <p class="mess-user>Comparte Conocimiento</p>
-      </div>
-    </form>
-    `;
-  divRegister.innerHTML = viewRegister;
-  const signupForm = divRegister.querySelector('#signup-form');
+const divRegister = document.createElement('div');
+const viewRegister = `
+<div class="contenedor">
+  <div id="signup-form" class="formulario">
+    <a href="#/login">
+      <i id="arrow" class="fas fa-chevron-left"></i>
+    </a>
+  </div>
+  <div>
+    <img class="logo" src="./imagen/logo_wup.jpg">
+  </div>
+  <div class="input-contenedor">
+    <i class="fas fa-envelope icon"></i>
+    <input class="input-init" type="email" id="signup-email" autocomplete="on" placeholder="E-mail" required />
+  </div>
+  <div class="input-contenedor">
+    <i class="fas fa-key icon"></i>
+    <input class="input-init" type="password" id="signup-password" placeholder="Contraseña"
+      oninvalid="setCustomValidity('Tu contraseña debe tener min. 6 caracteres')" required />
+  </div>
+  <div class="input-contenedor">
+    <input id="btn-register" type="submit" value="Registrarse" class="button">
+  </div>
+  <div>
+    <p>Ya estás registrado?<a href="#/login"> Inicia sesión aquí</a></p>
+    <p class="mess-user">Eleva tu Nivel</p>
+    <p class="mess-user">Crea Comunidad</p>
+    <p class="mess-user">Comparte Conocimiento</p>
+  </div>
+</div>
+`;
 
-  // id formulario de registro
-  const btnRegister = divRegister.querySelector('#btn-register');
-  btnRegister.addEventListener('click', (event) => {
-    event.preventDefault(); // para cancelar el evento del reinicio del formulario
-    // const signupUserName = document.querySelector('#singup-username').value;
-    const signupEmail = divRegister.querySelector('#signup-email').value;
-    const signupPassword = divRegister.querySelector('#signup-password').value;
-    // const signupPassword2 = document.querySelector('#signup-password2').value;
-    userNew(signupEmail, signupPassword);
-    // Limpiar el form
-    signupForm.reset();
-  });
-  return divRegister;
+divRegister.innerHTML = viewRegister;
+const signupForm = divRegister.querySelector('#signup-form');
+
+// id formulario de registro
+const btnRegister = divRegister.querySelector('#btn-register');
+btnRegister.addEventListener('click', (event) => {
+event.preventDefault(); // para cancelar el evento del reinicio del formulario
+// const signupUserName = document.querySelector('#singup-username').value;
+const signupEmail = divRegister.querySelector('#signup-email').value;
+const signupPassword = divRegister.querySelector('#signup-password').value;
+// const signupPassword2 = document.querySelector('#signup-password2').value;
+userNew(signupEmail, signupPassword);
+// Limpiar el form
+signupForm.reset();
+});
+return divRegister;
 };
