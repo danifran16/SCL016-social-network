3d122a3299f2b98f6692ba27a7a74523e91fb464
danifran16 <78364620+danifran16@users.noreply.github.com>
2021-06-06 14:28:09 -0400

    he corregido duplicado en #11
    Duplicado corregido, falta nombrar clases descriptivas en etiquetas div

--- src/lib/views/templateHome.js
+++ src/lib/views/templateHome.js
@@ -1,75 +1,53 @@
 import { createPost, showPost, signOff } from '../firebase.js';
 
 export const post = () => {
-  const divCreate = document.createElement('div');
-  const viewCreate = `
-  <div class = "contenedor">
+const divCreate = document.createElement('div');
+const viewCreate = `
+<div class="contenedor">
+  <div class="header-nav_home">
     <div>
-    <img class="logo-home" src="./imagen/logo_wup.jpg">
+      <img class="logo-home" src="./imagen/logo_wup.jpg">
     </div>
-        <h1>Bienvenido a WordUp<h1>
-        <h1>¡Comienza a Practicar!<h1>
-    <nav>
-    <select class="buttons-nav" name="Elige tu Idioma" id="selectLang">
-      <option value="español">Español</option>
-      <option value="ingles">Ingles</option>
-      <option value="frances">Frances</option>
-      <option value="aleman">Aleman</option>
-    </select>
-    <button type="button" id="buttonLogOut" class="buttons-nav2">
-    <span class="glyphicon glyphicon-log-out"></span> Log out
-    </nav>
-  <form id="taskForm">
-      <div class="formPost">
-        <textarea id="postNew" class="post-new" placeholder="Escribe tu Post"></textarea>
-        <button id="saveButton" class="button-2">WordUpear</button> 
-      </div>  
-
-  </form> 
-        <div class="get-post" id="getPost"></div>
+    <div>
+      <h2>Bienvenido a WordUp</h2>
+      <p>¡Comienza a Practicar!</p>
+    </div>
+    <div>
+      <select class="buttons-nav" name="Elige tu Idioma" id="selectLang">
+        <option value="español">Español</option>
+        <option value="ingles">Ingles</option>
+        <option value="frances">Frances</option>
+        <option value="aleman">Aleman</option>
+      </select>
+    </div>
+  </div>
+  <div>
+    <button type="button" id="buttonLogOut" class="buttons-nav2"></button>
+    <p class="glyphicon glyphicon-log-out">Log out</p>
+  </div>
+  <div class="formPost">
+    <textarea id="postNew" class="post-new" placeholder="Escribe tu Post"></textarea>
+    <button id="saveButton" class="button-2">WordUpear</button>
+  </div>
+  <div class="get-post" id="getPost"></div>
 </div>
 `;
 
-  divCreate.innerHTML = viewCreate;
+divCreate.innerHTML = viewCreate;
 
-  const newPost = divCreate.querySelector('#saveButton');
+const newPost = divCreate.querySelector('#saveButton');
 
-  newPost.addEventListener('click', () => {
-    const inputPost = divCreate.querySelector('#postNew').value;
-    createPost(inputPost);
-    showPost(inputPost);
-    // // const showNewPost = divCreate.querySelector('#getPost');
-    // // showNewPost.addEventListener('click', () => {
-    // //   showPost(inputPost);
-    // });
-  });
+newPost.addEventListener('click', () => {
+const inputPost = divCreate.querySelector('#postNew').value;
+createPost(inputPost);
+showPost(inputPost);
+});
 
-  // Para cerrar sesion
-  const logOut = divCreate.querySelector('#buttonLogOut');
-  logOut.addEventListener('click', () => {
-    signOff();
+// Para cerrar sesion
+const logOut = divCreate.querySelector('#buttonLogOut');
+logOut.addEventListener('click', () => {
+signOff();
+});
 
-    </form> 
-    <div id="getPost"></div>
-</div>
-`;
-  divCreate.innerHTML = viewCreate;
-
-  const newPost = divCreate.querySelector('#saveButton');
-  // const mostrarPost = document.getElementById('getPost');
-
-  newPost.addEventListener('click', () => {
-    const inputPost = divCreate.querySelector('#postNew').value;
-
-    // nuevoPost.innerHTML += '';
-    createPost(inputPost);
-    showPost(inputPost);
-    // const showNewPost = divCreate.querySelector('#getPost');
-    // showNewPost.addEventListener('click', () => {
-    //   showPost(inputPost);
-    // });
-
-  });
-
-  return divCreate;
+return divCreate;
 };
