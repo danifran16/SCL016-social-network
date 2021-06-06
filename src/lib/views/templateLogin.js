9050eef8b7ac19f1626dd744c0f7bbc41a41ace8
danifran16 <78364620+danifran16@users.noreply.github.com>
2021-06-06 15:12:17 -0400

    He corregido duplicado en #10
    Favor de corregir clases

--- src/lib/views/templateLogin.js
+++ src/lib/views/templateLogin.js
@@ -1,93 +1,54 @@
 import { googleProvider, singIn } from '../firebase.js';
 
 export const login = () => {
-  const divLogin = document.createElement('div');
+const divLogin = document.createElement('div');
 
-  const viewLogin = ` 
-<div id="root">
-  <div  id="contenedor" class="contenedor">
-    <div id="login-form" class="formulario">
-        <div>
-        <img class="logo" src="./imagen/logo_wup.jpg">
-        </div>
-          <div class="input-contenedor">
-          <i class="fas fa-envelope icon"></i>
-          <input class="input-init" type="email" id="login-email" placeholder="Correo Electronico">
-          </div>
-          <div class="input-contenedor">
-          <i class="fas fa-key icon"></i>
-          <input class="input-init" type="password" id="login-password" placeholder="Contraseña">
-          </div>
-          <div>
-          <input type="button" value="Ingresa" class="button" id="buttonSignIn">
-          </div>
-    </div>       
-    <div class="formulario-2">
-        <div class="input-contenedor-google">
-          <button id="login-google" class="btn-google">Click Aquí</button>
-          <img class="google-icons" src="./imagen/google.png">
-        </div>
-          <p>You do not have an account?:</p>
-          <a id="btn-register" href="#/register"><button class="check-in">Registrate aquí</button></a>
+const viewLogin = `
 
-  const viewLogin = `
-  <div id="contenedor" class="contenedor">
-
-    <div id="login-form" class="formulario">
-
-      <div>
-        <img class="logo" src="./imagen/logo_wup.jpg">
-      </div>
-
-      <div class="input-contenedor">
-        <i class="fas fa-envelope icon"></i>
-        <input class="input-init" type="email" id="login-email" placeholder="Correo Electronico">
-      </div>
-
-      <div class="input-contenedor">
-        <i class="fas fa-key icon"></i>
-        <input class="input-init" type="password" id="login-password" placeholder="Contraseña">
-      </div>
-
-      <div>
-        <input type="button" value="Ingresa" class="button" id="buttonSignIn">
-      </div>
-
+<div id="contenedor" class="contenedor">
+  <div id="login-form" class="formulario">
+    <div>
+      <img class="logo" src="./imagen/logo_wup.jpg">
     </div>
-
-
-    <div class="formulario-2">
-
-      <p>Or sign in with:</p>
-
-      <div class="input-contenedor-google">
-        <button id="login-google" class="btn-google"></button>
-        <img class="google-icons" src="./imagen/google.png">
-      </div>
-
-      <div>
-        <p>You do not have an account?:</p>
-        <a id="btn-register" href="#/register"><button class="check-in">Registrate aquí</button></a>
-      </div>
-
-
+    <div class="input-contenedor">
+      <i class="fas fa-envelope icon"></i>
+      <input class="input-init" type="email" id="login-email" placeholder="Correo Electronico" />
     </div>
-
+    <div class="input-contenedor">
+      <i class="fas fa-key icon"></i>
+      <input class="input-init" type="password" id="login-password" placeholder="Contraseña" />
+    </div>
+    <div>
+      <input type="button" value="Ingresa" class="button" id="buttonSignIn" />
+    </div>
   </div>
+  <div class="formulario-2">
+    <p>Or sign in with:</p>
+    <div class="input-contenedor-google">
+      <button id="login-google" class="btn-google"></button>
+      <img class="google-icons" src="./imagen/google.png"/>
+    </div>
+    <div>
+      <p>You do not have an account?:</p>
+        <a id="btn-register" href="#/register"><button class="check-in">
+          Registrate aquí</button></a>
+    </div>
+  </div>
+</div>
 `;
 
-  divLogin.innerHTML = viewLogin;
+divLogin.innerHTML = viewLogin;
 
-  const googleLogin = divLogin.querySelector('#login-google');
-  googleLogin.addEventListener('click', () => {
-    googleProvider();
-  });
+const googleLogin = divLogin.querySelector('#login-google');
+googleLogin.addEventListener('click', () => {
+googleProvider();
+});
 
-  const regularLogin = divLogin.querySelector('#buttonSignIn');
-  regularLogin.addEventListener('click', () => {
-    const email = document.getElementById('login-email').value;
-    const password = document.getElementById('login-password').value;
-    singIn(email, password);
-  });
-  return divLogin;
+const regularLogin = divLogin.querySelector('#buttonSignIn');
+regularLogin.addEventListener('click', () => {
+const email = document.getElementById('login-email').value;
+const password = document.getElementById('login-password').value;
+singIn(email, password);
+});
+return divLogin;
 };
